95c13e61ac9f288e6e2a506b397fa8b026754879
aryyya <arya@live.ca>
2018-04-07 17:04:55 -0700

    Added signup page.

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
 
-  test "full_title helper" do
+  test("full_title helper") do
     assert_equal(full_title, "Ruby on Rails Tutorial Sample App")
     assert_equal(full_title('Test'), "Test | Ruby on Rails Tutorial Sample App")
   end
