f8f3b8fe08638b40e472cf1a7243fdc2691b576f
aryyya <arya@live.ca>
2018-04-15 14:50:50 -0700

    Added friendly login forwarding, added users list.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   before_save { self.email.downcase! }
   validates(:name, presence: true, length: { maximum: 50 })
   validates(:email, presence: true, length: { maximum: 255 }, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false })
-  validates(:password, presence: true, length: { minimum: 8 })
+  validates(:password, presence: true, length: { minimum: 8 }, allow_nil: true)
   has_secure_password
 
   # Returns the hash digest of the given string.
---
+++ test/integration/users_edit_test.rb
@@ -0,0 +1,67 @@
+require 'test_helper'
+
+class UsersEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test('unsuccessful edit') do
+    log_in_as(@user)
+    get(edit_user_path(@user))
+    assert_template('users/edit')
+    patch(user_path(@user), params: {
+      user: {
+        name: '',
+        email: 'foo@invalid',
+        password: 'foo',
+        password_confirmation: 'bar'
+      }
+    })
+    assert_template('users/edit')
+    assert_select('div', text: 'The form contains 4 errors.')
+  end
+
+  test('successful edit') do
+    log_in_as(@user)
+    get(edit_user_path(@user))
+    assert_template('users/edit')
+    new_name = 'Foo Bar'
+    new_email = 'foo@bar.com'
+    patch(user_path(@user), params: {
+      user: {
+        name: new_name,
+        email: new_email,
+        password: '',
+        password_confirmation: ''
+      }
+    })
+    assert_not(flash.empty?)
+    assert_redirected_to(@user)
+    @user.reload
+    assert_equal(new_name, @user.name)
+    assert_equal(new_email, @user.email)
+  end
+
+  test('successful edit with friendly forwarding') do
+    get(edit_user_path(@user))
+    log_in_as(@user)
+    assert_redirected_to(edit_user_url(@user))
+    new_name = 'Foo Bar'
+    new_email = 'foo@bar.com'
+    patch(user_path(@user), params: {
+      user: {
+        name: new_name,
+        email: new_email,
+        password: '',
+        password_confirmation: ''
+      }
+    })
+    assert_not(flash.empty?)
+    assert_redirected_to(@user)
+    @user.reload
+    assert_equal(new_name, @user.name)
+    assert_equal(new_email, @user.email)
+  end
+
+end
